0ebe1a044a5757afe937c03287d3319e04daaf75
SM-mac <bluemix@sm-cloud.es>
2023-06-20 20:54:22 +0200

    Ethers example updated

--- Ethers-js/js/functions.js
+++ Ethers-js/js/functions.js
@@ -1,7 +1,5 @@
 import abi from './abi.js';
-
 let addr_contract = '0x71b5ebCe1dd6f529E7DD4F3a8C2ccAD5e610b71E';
-
 
 //////////////////////////////////////////////////////////////////////////////////
 // ETHERS JS
@@ -15,21 +13,19 @@ web3.send("eth_requestAccounts", []);
 const signer = web3.getSigner();
 */
 
-//export async function setNumber(addr_contract_, abi_, web3_) {
-
-export async function sign() {
+export async function sign(msg) {
   let web3 = new ethers.providers.Web3Provider(window.ethereum);
   // MetaMask requires requesting permission to connect users accounts
   web3.send("eth_requestAccounts", []);
   // The MetaMask plugin also allows signing transactions
   const signer = web3.getSigner();
 
-  let msg = "Hallo Welt";
   let signature = await signer.signMessage(msg);
   console.log(signature);
+  return signature;
 }
 
-export async function setNumber() {
+export async function setNumber(num) {
 
   let web3 = new ethers.providers.Web3Provider(window.ethereum);
   // MetaMask requires requesting permission to connect users accounts
@@ -37,61 +33,47 @@ export async function setNumber() {
   // The MetaMask plugin also allows signing transactions
   const signer = web3.getSigner();
 
-  // The DAI Contract is currently connected to the Provider,
+  // The Contract is currently connected to the Provider,
   // which is read-only. You need to connect to a Signer, so
   // that you can pay to send state-changing transactions.
 
   let sc = new ethers.Contract(addr_contract, abi, web3);
   let scWithSigner = sc.connect(signer);
-  let res = {};
-  res.sc = sc;
-  res.scWithSigner = scWithSigner;
+
+  let response = {};
 
   try {
 
-    let newVal = document.getElementById("myNumber").value;
+    let rawTX = await scWithSigner.setNumber(num);
 
-    document.getElementById("myNumber").value = ""; //Empty input
-
-    console.log(newVal);
-    let rawTX = await scWithSigner.setNumber(newVal);
-    // response = await scWithSigner.setNumber(99, {value: ethers.utils.parseEther("1.0")});
-    console.log("--- SEND ---");
+    console.log("--- RAW TX ---");
     console.log(rawTX);
     const receipt = await rawTX.wait(2);
 
-    console.log("--- TX ---");
+    console.log("--- CONFIRMED TX ---");
     console.log(receipt);
 
-    res.response = receipt;
-    res.result = true;
+    response.response = receipt;
+    response.result = true;
 
   } catch (error) {
     console.error(error);
-    res.error = error;
-    res.result = false;
+    response.error = error;
+    response.result = false;
     throw error;
   }
 
+  console.log(response);
 
-  console.log(res);
-
-
-
-  return res;
+  return response;
 
 }
 
 
-//export async function getNumber(addr_contract_, abi_, web3_) {
 export async function getNumber() {
 
   let web3 = new ethers.providers.Web3Provider(window.ethereum);
   let sc = new ethers.Contract(addr_contract, abi, web3);
-
-  // MetaMask requires requesting permission to connect users accounts
-  web3.send("eth_requestAccounts", []);
-
 
   let response;
 
@@ -102,7 +84,7 @@ export async function getNumber() {
 
   } catch (error) {
     console.error(error);
-    //return res.error = error;
+    
     throw error;
   }
 
